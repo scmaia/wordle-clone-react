13e7decae61dbb7c172556f426d7c3abee9fc6d0
Sara Costa-Maia <sara_cm88@hotmail.com>
2022-03-10 16:26:47 -0800

    initial app setup

---
+++ src/components/Letter/Letter.js
@@ -0,0 +1,11 @@
+import './Letter.scss';
+
+function Letter ( { letter, status } ) {
+    return (
+        <div>
+            <p>test</p>
+        </div>
+    );
+};
+
+export default Letter;
---
+++ src/components/LetterGrid/LetterGrid.js
@@ -0,0 +1,10 @@
+import './LetterGrid.scss';
+import Letter from "../Letter/Letter";
+
+function LetterGrid () {
+    return (
+        <Letter/>
+    );
+};
+
+export default LetterGrid;
